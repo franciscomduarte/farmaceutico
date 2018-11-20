c3249a189fc8f96ee5815913b8efbfb6c4ca858e
franciscomolina <franciscomolina@localhost>
2018-11-20 16:39:18 -0200

    Inicio do módulo de resposta e ajuste na parte exemplos para que possamos usar como base.

--- modulos/checklist-resposta/index.php
+++ modulos/checklist-resposta/index.php
@@ -1,89 +1,85 @@
 <?php 
 
-	define("NOME_MODULO", "Usuário"); 
-	define("NOME_ACAO", "Listar"); 
+	define("NOME_MODULO", "Questionários"); 
+	define("NOME_ACAO", "Responder"); 
 	include_once 'breadcrumb.php';
+	
+	$checklist = new Checklist();
+	$listaQuestionarios = $checklist->listar();
 
 ?>
         <div class="wrapper wrapper-content animated fadeInRight">
             <div class="row">
                 <div class="col-lg-12">
-	                <div class="ibox float-e-margins">
-	                    <div class="ibox-title col-lg-8">
-	                        <h5>Lista de Usuários</h5>
-	                    </div>
-	                   <div class="ibox-title-right col-lg-4">
-	                        <button type="button" class="btn btn-info" onclick="location.href='/usuario/novo/'">Novo</button>
-	                    </div>
 	                    <div class="ibox-content">
-	                        <div class="table-responsive">
-	                    		<table class="table table-striped table-bordered table-hover dataTables-example" >
-	                    			<thead>
-					                    <tr>
-					                        <th>Foto</th>
-											<th>Nome</th>
-					                        <th>CPF</th>
-					                        <th>E-mail</th>
-					                        <th>Data Cadastro</th>
-					                        <th>Perfil</th>
-					                        <th>Status</th>
-					                        <th><center>Ações</center></th>
-					                    </tr>
-	                    			</thead>
-	                   			 	<tbody>
-    									<?php 
-    										
-    										$usuario = new Usuario();
-    										#$array = $usuario->listar();
-    										
-    										foreach ($usuario->listar() as $usuario) {
-    										    
-    							        ?>
-        									<tr>
-        										<td width='25px'><img title="<?php echo $usuario->id ?>" class="img-circle m-t-xs img-responsive" src="/img/user.jpg"></td>
-        										<td><?php echo $usuario->nome?></td>
-        										<td><?php echo $usuario->cpf?></td>
-        										<td><?php echo $usuario->email?></td>
-        										<td><?php echo formatarDataHora($usuario->data_cadastro)?></td>
-        										<td><?php echo $usuario->perfil->descricao?></td>
-        										<td><?php echo $usuario->ativo ? "Ativo" : "Inativo"?></td>
-        										<td align="center">
-        										    <button onclick="visualizar(<?php echo $usuario->id?>)">
-        												<span class="glyphicon glyphicon-eye-open" title="Visualizar"></span>
-        											</button>
-        											<button onclick="editar(<?php echo $usuario->id?>)">
-        												<span class="glyphicon glyphicon-edit" title="Editar"></span>
-        											</button>
-        											<button onclick="excluir(<?php echo $usuario->id?>)">
-        												<span class="glyphicon glyphicon-trash remove" title="Excluir"></span>
-        											</button>
-        											
-        										</td>
-        									</tr>
-        				
-    									<?php 
-    							          	}
-    							        ?>
-	                    			</tbody>
-	                    			<tfoot>
-					                    <tr>
-											<th>Foto</th>
-											<th>Nome</th>
-					                        <th>CPF</th>
-					                        <th>E-mail</th>
-					                        <th>Data Cadastro</th>
-					                        <th>Perfil</th>
-					                        <th>Status</th>
-					                        <th><center>Ações</center></th>
-					                    </tr>
-	                    			</tfoot>
-	                    		</table>
-	                        </div>
+						<div class="ibox ">
+                        <div class="ibox-title">
+                            <h5>Questionários Disponíveis</h5>
+                        </div>
+                        <div class="ibox-content">
+
+                            <p class="m-b-lg">
+                                Escolha o questionário que deseja responder
+                            </p>
+							
+							<?php 
+							     foreach ($listaQuestionarios as $questionario) {
+							         $item = new Item();
+							         $itensChecklist = $item->listarPorIdCkelist($questionario->id);
+						    ?>
+                                <div class="dd" id="nestable2">
+                                    <ol class="dd-list">
+                                        <li class="dd-item" data-id="1">
+                                            <div class="dd-handle">
+                                            	<span class="pull-right"> <a href="#"><?php echo count($itensChecklist) . " questões" ?> </a></span>
+                                                <span class="label label-info"><i class="fa fa-users"></i></span> <?php echo $questionario->nome ?>
+                                            </div>
+                                        </li>
+                                    </ol>
+                                </div>
+                                
+                                <?php foreach ($itensChecklist as $i) { ?>
+                                	<div class="form-group">
+                                		<label class="control-label"><?php echo $i->enunciado ?><br/><small class="text-navy">Custom elements</small></label>
+                                	
+                                	<?php if ($i->tipo == 'ME')  { ?>
+                                    		<div class="i-checks"><label> <input type="checkbox" value=""> <i></i> Option one </label></div>
+                                            <div class="i-checks"><label> <input type="checkbox" value=""> <i></i> Option two checked </label></div>
+                                            <div class="i-checks"><label> <input type="checkbox" value=""> <i></i> Option three checked and disabled </label></div>
+                                            <div class="i-checks"><label> <input type="checkbox" value=""> <i></i> Option four disabled </label></div>
+                                	<?php } ?>
+                                	
+                                	<?php if ($i->tipo == 'TX')  { ?>
+                                		<input type="text" value="" class="form-control">
+                                	<?php } ?>
+                                	
+                                	<?php if ($i->tipo == 'VF')  { ?>
+										<div class="i-checks"><label> <input type="radio" value="option1" name="a"> <i></i> SIM </label></div>
+                                        <div class="i-checks"><label> <input type="radio" value="option2" name="a"> <i></i> NÃO </label></div>
+                                	<?php } ?>
+                                	
+                                	<?php if ($i->tipo == 'MV')  { ?>
+                                		<select multiple="multiple" class="form-control">
+                                			<option>Opção 1</option>
+                                			<option>Opção 2</option>
+                                			<option>Opção 3</option>
+                                		</select>
+                                	<?php } ?>
+                                	
+                                	<div class="hr-line-dashed"></div>
+                                <?php } ?>
+                                
+                            <?php } ?>
+                            
+
+
 	                    </div>
 	                </div>
 	            </div>
             </div>
         </div>
+     </div>
+  </div>
         
 		<script>
     		function visualizar(id){
