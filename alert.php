0e7a980c23a972f397a25ff7cecf35e83ef8ef5a
franciscomolina <franciscomolina@localhost>
2018-11-23 23:49:29 -0200

    Testes de novos componentes

---
+++ alert.php
@@ -0,0 +1,40 @@
+<div class="sweet-overlay" tabindex="-1"
+	style="opacity: 1.1; display: block;"></div>
+<div class="sweet-alert showSweetAlert visible" data-custom-class=""
+	data-has-cancel-button="false" data-has-confirm-button="true"
+	data-allow-outside-click="false" data-has-done-function="false"
+	data-animation="pop" data-timer="null"
+	style="display: block; margin-top: -127px;">
+	<div class="sa-icon sa-error" style="display: none;">
+		<span class="sa-x-mark"> <span class="sa-line sa-left"></span> <span
+			class="sa-line sa-right"></span>
+		</span>
+	</div>
+	<div class="sa-icon sa-warning" style="display: none;">
+		<span class="sa-body"></span> <span class="sa-dot"></span>
+	</div>
+	<div class="sa-icon sa-info" style="display: none;"></div>
+	<div class="sa-icon sa-success" style="display: none;">
+		<span class="sa-line sa-tip"></span> <span class="sa-line sa-long"></span>
+
+		<div class="sa-placeholder"></div>
+		<div class="sa-fix"></div>
+	</div>
+	<div class="sa-icon sa-custom" style="display: none;"></div>
+	<h2>Welcome in Alerts</h2>
+	<p style="display: block;">Lorem Ipsum is simply dummy text of the
+		printing and typesetting industry.</p>
+	<fieldset>
+		<input type="text" tabindex="3" placeholder="">
+		<div class="sa-input-error"></div>
+	</fieldset>
+	<div class="sa-error-container">
+		<div class="icon">!</div>
+		<p>Not valid!</p>
+	</div>
+	<div class="sa-button-container">
+		<button class="cancel" tabindex="2" style="display: none;">Cancel</button>
+		<button class="confirm" tabindex="1"
+			style="display: inline-block; background-color: rgb(174, 222, 244); box-shadow: rgba(174, 222, 244, 0.8) 0px 0px 2px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px inset;">OK</button>
+	</div>
+</div>
---
+++ modal.php
@@ -0,0 +1,39 @@
+
+<div class="modal inmodal fade" id="myModal5" tabindex="-1"
+	role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">
+					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+				</button>
+				<h4 class="modal-title">Modal title</h4>
+				<small class="font-bold">Lorem Ipsum is simply dummy text of the
+					printing and typesetting industry.</small>
+			</div>
+			<div class="modal-body">
+				<p>
+					<strong>Lorem Ipsum is simply dummy</strong> text of the printing
+					and typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer
+					took a galley of type and scrambled it to make a type specimen
+					book. It has survived not only five centuries, but also the leap
+					into electronic typesetting, remaining essentially unchanged.
+				</p>
+				<p>
+					<strong>Lorem Ipsum is simply dummy</strong> text of the printing
+					and typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer
+					took a galley of type and scrambled it to make a type specimen
+					book. It has survived not only five centuries, but also the leap
+					into electronic typesetting, remaining essentially unchanged.
+				</p>
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
+</div>
