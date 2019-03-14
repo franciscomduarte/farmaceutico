6cb94cf8141a1b60523dc3e22f5aba62a54454cc
Flaviano O. Silva <fosbsb@gmail.com>
2019-03-14 12:32:56 -0300

    Configuração para produção

--- config.php
+++ config.php
@@ -14,13 +14,13 @@ define('AMBIENTE','PROD');
 $ip = getenv("REMOTE_ADDR");
 if($ip == '127.0.0.1' && AMBIENTE == "DEV") {
     define('HOST', 'localhost');
-    define('DBNAME', 'e2f10_desenvolvimento');
+    define('DBNAME', 'e2f10');
     define('CHARSET', 'utf8');
     define('USER', 'root');
     define('PASSWORD', '');
 } else {
     define('HOST', 'mysql.e2f.com.br');
-    define('DBNAME', 'e2f10_desenvolvimento');
+    define('DBNAME', 'e2f10');
     define('CHARSET', 'utf8');
     define('USER', 'e2f10');
     define('PASSWORD', 'e2f12345678');
